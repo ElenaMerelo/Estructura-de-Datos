76abd1c385841181aefae5a92884e222fc195c84
Elena Merelo <ElenaMerelo@users.noreply.github.com>
2017-11-24 16:50:52 +0100

    Update pila_max_cola.h
    Lo pongo bonitoh

--- Prácticas/Práctica 3 (Todo)/include/pila_max_cola.h
+++ Prácticas/Práctica 3 (Todo)/include/pila_max_cola.h
@@ -1,10 +1,9 @@
 /**
   * @file pila.h
   * @brief Fichero cabecera del TDA Pila_Max
-  *
-  * Gestiona una secuencia de elementos con facilidades para la inserci�n y
-  * borrado de elementos en un extremo
-  *
+  * @author Elena Merelo Molina y Antonio Gámiz Delgado
+  * @date Noviembre de 2017
+
   */
 
 #ifndef __Pila_H__
@@ -20,26 +19,23 @@
  *
  * Una instancia @e v del tipo de datos abstracto Pila sobre el tipo @c T es 
  * una lista de elementos del mismo con un funcionamiento @e LIFO (Last In, 
- * First Out). En una pila, las operaciones de inserci�n y borrado de elementos
+ * First Out). En una pila, las operaciones de inserción y borrado de elementos
  * tienen lugar en uno de los extremos denominado @e Tope. Una pila de longitud
  * @e n la denotamos
 
  * - [a1,a2,a3,..,an>
 
- * donde @e ai es el elemento de la posici�n i-�sima.
+ * donde @e ai es el elemento de la posición i-ésima.
 
- * En esta pila, tendremos acceso �nicamente al elemento del @e Tope, es decir, 
- * a @e an. El borrado o consulta de un elemento ser� sobre  @e an, y la
- * inserci�n de un nuevo elemento se har� sobre �ste. Quedando el elemento
+ * En esta pila, tendremos acceso únicamente al elemento del @e Tope, es decir, 
+ * a @e an. El borrado o consulta de un elemento será sobre  @e an, y la
+ * inserción de un nuevo elemento se hará sobre éste. Quedando el elemento
  * insertado como @e Tope de la pila.
 
- * Si @e n=0 diremos que la pila est� vac�a.
+ * Si @e n=0 diremos que la pila está vacía.
 
- * El espacio requerido para el almacenamiento es O(n). Donde n es el n�mero de
+ * El espacio requerido para el almacenamiento es O(n). Donde n es el número de
  * elementos de la Pila.
- *
- * @author Elena Merelo Molina y Antonio Gámiz Delgado.
- * @date Noviembre 2017
  */
 
 struct elemento{
@@ -56,6 +52,7 @@ class Pila_max{
          * @brief Constructor por defecto
          */
         Pila_max(){};
+ 
         /**
          * @brief Constructor de copias
          * @param otra La pila de la que se har� la copia.
@@ -64,35 +61,40 @@ class Pila_max{
 
         // --------------- Otras funciones ---------------
         /**
-         * @brief Operador de asignaci�n
+         * @brief Operador de asignación
          * @param otra La pila que se va a asignar.
          */
         Pila_max& operator= (const Pila_max& other){queue = other.queue;}
+ 
         /**
-         * @brief Comprueba si la pila est� vac�a
+         * @brief Comprueba si la pila está vacía
          */
         bool empty() const{ return queue.vacia(); }
+ 
         /**
          * @brief Devuelve el elemento del tope de la pila
          */
         elemento top ();
+ 
         /**
          * @brief Devuelve el elemento del tope de una pila constante
          */
         const elemento top () const;
+ 
         /**
-         * @brief A�ade un elemento "encima" del tope de la pila
-         * @param elem Elemento que se va a a�adir.
+         * @brief Añade un elemento "encima" del tope de la pila
+         * @param elem Elemento que se va a añadir.
          */
         void push(int num);
+ 
         /**
          * @brief Quita el elemento del tope de la pila
          */
         void pop();
         /**
-         * @brief Devuelve el n�mero de elementos de la pila
+         * @brief Devuelve el número de elementos de la pila
          */
         int size() const{ return queue.num_elementos(); }
 };
 
-#endif // __Pila_H__
+#endif
