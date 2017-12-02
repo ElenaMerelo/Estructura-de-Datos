83898fb9750f32498ba9945cf2a517ab2bae0c9c
Antonio <antoniogamiz10@gmail.com>
2017-12-02 20:45:41 +0100

    programa de prueba de cronologia

--- Prácticas/Práctica 4/src/cronologia.cpp
+++ Prácticas/Práctica 4/src/cronologia.cpp
@@ -10,7 +10,21 @@ typedef pair<int, fecha_historica> par;
 
 //Implementar.
 void cronologia::addEvent(fecha_historica& f){
-    data.insert(par(f.getAge(), f));
+    
+    cronologia::it i = data.find(f.getYear());
+
+    if( i != data.end() ){
+
+        fecha_historica aux;
+        aux.setYear(f.getYear());
+        f.unionEventos((*i).second,aux);
+
+        data.erase(i);
+        data.insert(par(f.getYear(),aux));
+    } 
+    else{
+       data.insert(par(f.getYear(), f));
+    }
 }
 
 //Devuelve una fecha histórica en la cronología con año a.
@@ -19,6 +33,25 @@ fecha_historica& cronologia::search_fecha_historica(int a){
         return data.find(a)->second;
     fecha_historica f;
     return f;
+}
+
+//Une dos cronologías.
+void cronologia::unionCronologias(const cronologia &f, cronologia &u){
+    u=f;
+    cronologia::it i;
+    for(i=data.begin(); i!=data.end(); ++i){
+        u.addEvent((*i).second);
+    }
+}
+
+//Interseca dos cronologías.
+void cronologia::interseccionCronologias(const cronologia& c, cronologia &u){
+    cronologia::it i;
+    for(i=data.begin(); i!=data.end(); ++i){
+        if( c.data.count((*i).second.getYear()) > 0 )
+            u.addEvent((*i).second);
+    }
+
 }
 
 //Operador de salida.
---
+++ Prácticas/Práctica 4/src/cronologia_test.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<cronologia.h>
+#include<fstream>
+using namespace std;
+
+int main(int argc, char * argv[]){
+
+    ifstream f1(argv[1]);
+    if(!f1){
+        cout << "Error al abrir el archivo" << endl;
+        return 1;
+    }
+
+    ifstream f2(argv[2]);
+    if(!f2){
+        cout << "Error al abrir el archivo" << endl;
+        return 1;
+    }
+
+    cronologia a;
+    cronologia b;
+
+    f1 >> a;
+    f2 >> b;
+
+    // cout << a << endl;
+    // cout << b << endl;
+
+    // cronologia u;
+    // a.unionCronologias(b,u);
+
+    // cout << u << endl;
+
+    cronologia i;
+    a.interseccionCronologias(b,i);
+
+    cout << i << endl;
+
+    // cout << a.search_fecha_historica(19334) << endl;
+
+    return 0;
+}
+
