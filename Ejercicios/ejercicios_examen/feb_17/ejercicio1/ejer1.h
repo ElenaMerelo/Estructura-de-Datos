7cf7e8d89242e93dad33238315b7b881fe4e7a8a
Elena Merelo <nenamer@gmail.com>
2017-12-24 13:13:06 +0100

    Añado ejercicio 2. Da error en el operator<<, I´m working to fix that

---
+++ Ejercicios/ejercicios_examen/feb_17/ejercicio1/ejer1.h
@@ -0,0 +1,37 @@
+#include <stack>
+#include <iostream>
+
+using namespace std;
+
+#ifndef EJER1_H
+#define EJER1_H
+
+template <class T>
+class pila_doble{
+private:
+  stack<T> pila0, pila1;
+public:
+  //Constructor vacío
+  pila_doble(){}
+
+  //Constructor de una pila doble a con el elemento element size veces
+  pila_doble(int size, T element);
+
+  //Constructor de copia
+  pila_doble(pila_doble<T> &other);
+
+  //Operador de asignación
+  pila_doble& operator=(pila_doble<T> &other);
+
+  //Método que elimina el elemento tope de una pila u otra según el índice pasado como argumento
+  void pop(int indice_pila);
+
+  //Método que inserta un elemento en una pila u otra según el entero pasado como parámetro
+  void push(int indice_pila, T element);
+
+  //Método que añade un elemento al final de la pila doble, esto es, al principio de la pila 1
+  void push(T element);
+
+};
+#endif
+#include <ejer1.cpp>
---
+++ Ejercicios/ejercicios_examen/feb_17/ejercicio1/main.cpp
@@ -0,0 +1,14 @@
+#include <ejer1.h>
+#include <stack>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  pila_doble<int> p;
+
+  for(int i= 0; i< 5; i++)
+    p.push(i+1);
+
+  cout<< p;
+}
